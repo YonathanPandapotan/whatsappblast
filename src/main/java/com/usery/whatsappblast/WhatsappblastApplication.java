fb37cb0109d93a9c7546a8d23b7ad12382085230
YonathanPandapotan <yonathan_pandapotan@yahoo.com>
2023-06-14 09:36:01 +0700

    test push to master with security

--- src/main/java/com/usery/whatsappblast/WhatsappblastApplication.java
+++ src/main/java/com/usery/whatsappblast/WhatsappblastApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class WhatsappblastApplication {
 
 	public static void main(String[] args) {
+		// Comment
 		SpringApplication.run(WhatsappblastApplication.class, args);
 	}
 
